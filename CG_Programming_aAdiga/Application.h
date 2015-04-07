7233bc6480668b39a61eaec36d6f18dc4ce31900
unknown <r202-stu@AI241LAB0202P16.aipdcls.edu>
2015-04-06 21:35:34 -0700

    first commit

---
+++ CG_Programming_aAdiga/Application.h
@@ -0,0 +1,12 @@
+#pragma once 
+
+#include <cmath>
+#include <stdio.h>
+#include <iostream>
+#include <cassert>
+#include <stdexcept>
+#include <stdlib.h>
+
+using namespace std;
+
+
---
+++ CG_Programming_aAdiga/Source.cpp
@@ -0,0 +1,12 @@
+#include "Application.h"
+
+int main (){
+
+cout << "hello" << endl;
+
+do{
+}while(true);
+
+
+
+}
